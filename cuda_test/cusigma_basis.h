110aa3d26ea466b297eaca0604a7a3ff884c9460
wilthoma <t.willwacher@gmail.com>
2025-05-14 23:46:04 +0200

    ...

--- cuda_test/cusigma_basis.h
+++ cuda_test/cusigma_basis.h
@@ -149,7 +149,7 @@ std::vector<int64_t> _cuPM_Basis(
         CHECK_CUDA(cudaMalloc(&mul1, n * n * (d/2) * sizeof(u64)));
         // auto GG = poly_mat_mul_fft_red(MM, G, seqprime, 0, d + 1);
         cupoly_mat_mul_fft_gpu(dout1, dG, mul1, n, n, n/2, d/2+1, d, d/2, d/2);
-        modp_buffer(mul1, n * n * (d/2), seqprime);
+        modp_buffer(mul1, n * (n/2) * (d/2), seqprime);
         // shift_trunc_in(GG, d / 2, d / 2);
 
         u64* dout2;
